bd10050210be89b5cef26fd196559a2ae4e36fb6
beezay bohora <bijay.okur@gmail.com>
2021-02-19 12:39:15 +0545

    task 3 completed

--- task3/main3.js
+++ task3/main3.js
@@ -1,4 +1,9 @@
-let date = new Date();
+/**Catching DOM Elements ****/
+const btn = document.getElementById('timeBtn');
+const modal = document.getElementById('display');
+
+const getDate = () => {
+  let date = new Date();
 
 const addZero = (d) => {
     if(d <= 9){
@@ -7,6 +12,15 @@ const addZero = (d) => {
     return d
   }
 
-formatDate = `"${date.getFullYear()}-${addZero(date.getMonth() +1)}-${addZero(date.getDay())}"`
+formatDate = `"${date.getFullYear()}-${addZero(date.getMonth() +1)}-${addZero(date.getDay())}"`;
+
+modal.innerHTML = `<h5>The New Date is: <span>${formatDate}</span></h5>`
 
 console.log('Formatted Date => ', formatDate);
+}
+
+btn.addEventListener('click', () => {
+  getDate();
+  console.log('Button Click');
+})
+
