944f35dc8d50b4502fbf87e152c9afa5ff22b1d3
beezay bohora <bijay.okur@gmail.com>
2021-02-19 13:50:44 +0545

    time display

--- task3/main3.js
+++ task3/main3.js
@@ -1,26 +1,58 @@
 /**Catching DOM Elements ****/
-const btn = document.getElementById('timeBtn');
+const dateBtn = document.getElementById('dateBtn');
+const timeBtn = document.getElementById('timeBtn');
 const modal = document.getElementById('display');
-
-const getDate = () => {
-  let date = new Date();
+// const timeDisplay = document.getElementById('time-')
+// const date = new Date();
+let t;
 
 const addZero = (d) => {
-    if(d <= 9){
-      return "0" + d;
-    }
-    return d
+  if (d <= 9) {
+    return "0" + d;
   }
-
-formatDate = `"${date.getFullYear()}-${addZero(date.getMonth() +1)}-${addZero(date.getDay())}"`;
-
-modal.innerHTML = `<h5>The New Date is: <span>${formatDate}</span></h5>`
-
-console.log('Formatted Date => ', formatDate);
+  return d
 }
 
-btn.addEventListener('click', () => {
+// const removeTimeEvent = () => {
+//   timeBtn.removeEventListener('click', getTime())
+// }
+
+const getDate = () => {
+
+  // timeBtn.removeEventListener('click', getTime())
+  clearInterval(t);
+
+  const date = new Date();
+
+  formatDate = `"${date.getFullYear()}-${addZero(date.getMonth() +1)}-${addZero(date.getDay())}"`;
+
+  modal.innerHTML = `<h5>The New Date is: <span>${formatDate}</span></h5>`
+
+  console.log('Formatted Date => ', formatDate);
+}
+
+const getTime = () => {
+  const date = new Date();
+
+  formatTime = `"${addZero(date.getHours())}:${addZero(date.getMinutes())}:${addZero(date.getSeconds())}"`
+  modal.innerHTML = `<h5>The New Time is: <span>${formatTime}</span></h5>`;
+  console.log('Formatted Time => ', formatTime);
+}
+
+dateBtn.addEventListener('click', () => {
   getDate();
   console.log('Button Click');
 })
 
+
+
+timeBtn.addEventListener('click', () => {
+  getTime();
+  t = setInterval(getTime, 1000);
+  console.log('Button Click');
+})
+
+// const timeDisplay = () => {
+//   getTime();
+//   setInterval(getTime, 1000);
+// }
