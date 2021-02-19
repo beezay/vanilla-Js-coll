ff409c292679a0d3a0c152beb2207b161b361316
beezay bohora <bijay.okur@gmail.com>
2021-02-19 16:33:23 +0545

    multiple search added

--- task4/main4.js
+++ task4/main4.js
@@ -1,27 +1,93 @@
-const coinName = ["Bitcoin","DogeCoin","Litecoin","Ethereum","Binance Coin"];
+const coinName = ["Bitcoin", "DogeCoin", "Litecoin", "Ethereum", "Binance Coin"];
 
-const coinPrice = ['$52','$126','$0.0526','$232.06','$1,909.77','$144.048'];
+const coinPrice = ['$52', '$126', '$0.0526', '$232.06', '$1,909.77', '$144.048'];
 
-let inputName = ['LiTeCoIN', 'BitCoin', 'ETHEREUM']
+let inputName = []
 let indexColl = [];
 let result = [];
-const search = (element) => {
-    return element.toLowerCase() === inputName.toLowerCase();
+
+//!!****Handling Button AND UI ******///
+const singleBtn = document.getElementById('singleBtn');
+const multipleBtn = document.getElementById('multipleBtn');
+const singleInput = document.getElementById('singleInput');
+const multipleInput = document.getElementById('multipleInput');
+//??Event Listeners to Buttons
+singleBtn.addEventListener('click', () => {
+    singleBtn.classList.remove('btn-close-white')
+    singleInput.style.display = 'block';
+    multipleInput.style.display = 'none'
+    multipleBtn.classList.add('btn-close-white');
+    inputName = [];
+})
+
+multipleBtn.addEventListener('click', () => {
+    multipleBtn.classList.remove('btn-close-white');
+    singleInput.style.display = 'none';
+    multipleInput.style.display = 'block';
+    singleBtn.classList.add('btn-close-white')
+    inputName = [];
+})
+
+const tableBody = document.getElementById('table-body');
+
+//!!****Handling Inputs and Functions ******///
+singleInput.addEventListener('change', () => {
+    inputName = [];
+    console.log('Single Input => ', singleInput.value);
+    inputName.push(singleInput.value);
+    output();
+    displayRes();
+    console.log('Result => ', result);
+})
+
+multipleInput.addEventListener('change', () => {
+    inputName = [];
+    console.log('Multiple Input => ', multipleInput.value);
+    let inputTxt = multipleInput.value;
+    inputName = inputTxt.split(',');
+    output();
+    displayRes();
+    console.log('CheckArray=>', inputName);
+})
+
+
+// const search = (element) => {
+//     return element.toLowerCase() === inputName.toLowerCase();
+// }
+
+const output = () => {
+    console.log('Result @ beginning=>', result)
+    indexColl = [];
+    inputName.forEach(input => {
+        indexNumber = coinName.findIndex((element) => {
+            return element.toLowerCase() === input.toLowerCase();
+        });
+        indexColl.push(indexNumber);
+    })
+
+    
+    // console.log('Index Collection', indexColl, 'result=> ', result);
+
+    indexColl.forEach(index => {
+        result.push( {
+            CryptoName: coinName[index],
+            CryptoPrice: coinPrice[index]
+        })
+        console.log('Index Loop =>', index, result)
+    })
 }
 
-inputName.forEach(input => {
-    indexNumber = coinName.findIndex((element) => {
-        return element.toLowerCase() === input.toLowerCase();
-    });
-    indexColl.push(indexNumber);
-})
+const displayRes = () => {
+    tableBody.innerHTML = '';
+    console.log('Display Res=> ', result)
+    for (i = 0; i < result.length; i++) {
+        res = result[i];
+        let displayResult = `<tr>
+                            <th>${res.CryptoName}</th>
+                            <th>${res.CryptoPrice}</th>
+                        </tr>`;
+        tableBody.innerHTML += displayResult
+    }
+}
 
-indexColl.forEach(index => {
-    result.push({
-        CryptoName: coinName[index],
-        CryptoPrice: coinPrice[index]
-    })
-})
-
-
-console.log('Result => ', result);
+// console.log('Result => ', result);
